c9c7321c72a467be2de045f80fa40f0b6937b5e3
k-ravuri <kaviravuri888@gmail.com>
2018-08-15 14:08:16 -0700

    stuff

--- event.js
+++ event.js
@@ -4,27 +4,6 @@ function logOut(e) {
  }
 
 
- // Save message to firebase
-function saveMessage(fname, lname, company, email, password){
-  var newComapnyChild = messagesRef.child(company)
-  var newMessageRef = newComapnyChild.push();
-  newMessageRef.set({
-    fname: fname,
-    lname: lname,
-    email:email,
-    organizer:false,
-    password:password,
-  });
-}
-
-
-function createEvent(e){
-  var eventName = getInputVal('eventName');
-  console.log(firebase.auth().currentUser);
-  //var company = getCompany(firebase.auth().currentUser);
-  var email = getInputVal('email').toLowerCase();
-  var password = getInputVal('password');
-
-  // Save message
-  //saveMessage(fname, lname, company, email, password); 
-}
+ function eventPageRedirect(e) {
+    location.href = "https://www.plumdrop.co/ibm";
+ }
